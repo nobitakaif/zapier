d26da6b69f03afed6b5a2e65cdc305647b536669
mohd kaif <mk2818356@gmail.com>
2025-07-20 22:28:03 +0530

    connected hooks with db

--- apps/hooks/index.ts
+++ apps/hooks/index.ts
@@ -1,9 +1,37 @@
 import express from "express"
-import { prismaClient } from "db/clinet"
+import { prismaClient } from "db/client"
 
 const app = express()
 app.use(express.json())
 
-app.post('/hooks/catch/:',async (req,res)=>{
 
+app.post("/hooks/catch/:userId/:zapId",async (req,res)=>{
+    const userId = req.params.userId
+    const zapId = req.params.zapId
+    const body = req.body
+    
+    // this is transaction in database either both will done or none of these happens
+    await prismaClient.$transaction(async tx => {
+        const res = await tx.zapRun.create({
+            data:{
+                zapId : zapId,
+                metaData : body
+            }
+        })
+
+        await tx.zapRunOutbox.create({
+            data:{
+                zapRunId : res.id
+            }
+        })
+        
+    })
+
+    res.json(200).json({
+        msg:"alrigh"
+    })
+})
+
+app.listen(8000,()=>{
+    console.log("server is running or port 8000")
 })
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,22 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client"
 
 
 export const prismaClient = new PrismaClient()
 
+
+// async function main (){
+//         try{
+//             const res  = await prismaClient.user.create({
+//                 data:{
+//                     username:"kaif",
+//                     email:"kaif@gmail.com",
+//                     password:"nobitakaif"
+//                 }
+//             })
+//             console.log(res.id)
+            
+//         }catch(e){
+//             console.log("something happens wrong",e)
+//         }
+// }
+// main()
